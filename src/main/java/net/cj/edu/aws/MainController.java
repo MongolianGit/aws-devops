2b9a2ae4837a983710151f9be0da33850bbcb78f
CJ <CJ@DESKTOP-QE9FGB2>
2022-11-11 14:32:29 +0900

    메인 페이지 생성

---
+++ src/main/java/net/cj/edu/aws/MainController.java
@@ -0,0 +1,17 @@
+package net.cj.edu.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+
+@RestController
+
+public class MainController {
+	
+	
+	@GetMapping("/")
+	public String home() {
+		return "<h1>aws-devops</h1>"; 
+	}
+
+}
